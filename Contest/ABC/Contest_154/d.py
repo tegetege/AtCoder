6d2507f2e856aa5ce76b5873e4befb6ea1044411
tegetege <m_take7_ex_d@yahoo.co.jp>
2020-02-09 23:31:00 +0900

    Today's Activity

--- Contest/ABC/Contest_154/d.py
+++ Contest/ABC/Contest_154/d.py
@@ -1,4 +1,5 @@
 '''
+- 累積和
 期待値
 E[X] = sum(1:N)/N
 '''
@@ -6,15 +7,18 @@ E[X] = sum(1:N)/N
 N,K = map(int,input().split())
 p = list(map(int,input().split()))
 
-# 期待値のリスト生成
-ex_list = list()
+# 期待値の累積和リストを生成
+ex_list = list() 
 for i in range(len(p)):
-	ex_list.append(sum(range(1,p[i]+1))/p[i])
+	if i != 0:
+		ex_list.append(((1+p[i])/2)+ex_list[-1])
+	else:
+		ex_list.append((1+p[i])/2)
 
-ans = 0
-for i in range(N-K+1):
-	ex = sum(ex_list[i:i+K])
+ans = ex_list[K-1]
+for i in range(N-K):
+	# print(i,ex_list[i],ex_list[i+K])
+	ex = abs(ex_list[i] - ex_list[i+K])
 	if ans < ex:
 		ans = ex
-
 print(ans)
