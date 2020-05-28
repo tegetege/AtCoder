963c4877cf3a31325003c51bcccc75405134142a
tegetege <m_take7_ex_d@yahoo.co.jp>
2020-05-29 00:23:54 +0900

    today's Activity

---
+++ Practice/APG4b/1_06.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int A, B;
+  string op;
+  cin >> A >> op >> B;
+
+  if (op == "+") {
+    cout << A + B << endl;
+  }
+
+  else if (op == "-") {
+    cout << A - B << endl;
+  }
+
+  else if (op == "*") {
+    cout << A * B << endl;
+  }
+
+  else if (B == 0) {
+    cout << "error" << endl;
+  }
+
+  else if (op == "/") {
+    cout << A / B << endl;
+  }
+
+  else {
+    cout << "error" << endl;
+  }
+
+}
