725d5a6d60c2132273889ca9f73c35845772d1ba
tegetege <24471423+tegetege@users.noreply.github.com>
2020-10-14 23:56:36 +0900

    fix  a_frog1

--- Practice/DP/a_frog1.cpp
+++ Practice/DP/a_frog1.cpp
@@ -6,20 +6,17 @@ typedef long long ll;
 #define rep(i, n) for (int i = 1; i <= (int)(n); i++)
 
 int main() { 
-    ll N, inf = 10000007;
+    ll N, inf = 10000007777;
     cin >> N;
-    vector<int> h(N+1), DP(N+1, inf);
+    vector<ll> h(N+1), DP(N+1, inf);
     rep(i, N) {
         cin >> h.at(i);
     }
 
     DP[1] = 0;
-    for(int i = 2; i <= N; ++i) {
-        DP[i] = min(DP[i], DP[i-1] + abs(h[i]-h[i-1]));
-        if (i >= 3) {
-            DP[i] = min(DP[i], DP[i-2] + abs(h[i]-h[i-2]));
-        }
-        // cout << DP[i] << "\n";
+    DP[2] = abs(h[2] - h[1]);
+    for(int i = 3; i <= N; ++i) {
+        DP[i] = min(DP[i-1] + abs(h[i]-h[i-1]), DP[i-2] + abs(h[i]-h[i-2]));
     }
     cout << DP[N] << "\n";
 
