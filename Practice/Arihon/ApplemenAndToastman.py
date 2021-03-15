ebf3a84ee2e7ffdf4e24f4000c69e78327a3a3cd
tegetege <24471423+tegetege@users.noreply.github.com>
2021-03-15 23:53:15 +0900

    Today's Activity

---
+++ Practice/Arihon/ApplemenAndToastman.py
@@ -0,0 +1,28 @@
+# http://codeforces.com/contest/462/problem/C
+"""
+操作1
+・毎回、Toastmanはグループの数字を得て、全ての数字を足し合わせ、スコアに足すことができる。
+・その時、ToastmanはグループをApplemanに渡します
+
+操作2
+・毎回、Applemanはグループの中の1つの数字を得て、グループの外に出します。
+・Applemanはグループの中から1つ以上の数字を得るたびに、2つの空ではないグループに分けて、
+　それぞれをToastmanに渡します。
+"""
+
+N = int(input())
+a = list(map(int, input().split()))
+
+a.sort() # インプットを昇順にソート
+ans = 0
+a_sum = sum(a)
+while len(a) > 0:
+
+    ans += a_sum # Toastmanの操作
+    b = a.pop(0) # Appleman split操作: 一番小さい数字だけsplitする
+    a_sum -= b
+    if len(a) != 0:
+        # 最後の1回の時に余分に足されるのを防ぐ
+        ans += b # bだけToastmanに渡す
+
+print(ans)
